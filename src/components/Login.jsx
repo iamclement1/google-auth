fa7479e9feea2c03b22ef2eff0f2e01c10fa150b
iamclement1 <blessedmarcel1@gmail.com>
2023-02-14 12:50:47 +0100

    fetch user data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,34 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { app } from '../services/firebase';
 import './login.css';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 const Login = () => {
 
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+    const [ { user } , dispatch ] = useStateValue();
     const LoginButton = async () => {
-        const response = await signInWithPopup(firebaseAuth, provider)
-        console.log(response);
-    };
+        const { user : { refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+        //dispatch the data 
+
+        dispatch({
+            type: actionType.SET_USER, //setting the action type from the reducer hook so we call the action type object
+            user : providerData[0]
+        })
+    }
+
+
     return (
         <div>
             <button className="login__button" onClick={LoginButton}><i className="fab fa-google"></i>
-            Login with Google</button>
+            {/* here we check if a user is logged in the "login with Google" button should display otherwise display the 'user name' found in the Google account */}
+                {
+                    user ? user.displayName : 'Login with Google'
+                }
+            </button>
         </div>
     )
 }
